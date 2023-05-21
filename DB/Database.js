007da6e2a0e31288bb6d743af15e79fa1ba1a319
Swati Namdev <chirag awasthi>
2023-05-21 11:09:09 +0530

    test

--- DB/Database.js
+++ DB/Database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async (req, res) => {
     // const db = process.env.MONGO_URL;
 
-    const {connection} = await mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+    const {connection} = await mongoose.connect("mongodb+srv://swatimern:swatimern@mern.ovdqy7z.mongodb.net/stocksense?retryWrites=true&w=majority", { useNewUrlParser: true });
 
     console.log(`MongoDB Connected to ${connection.host}`);
 
