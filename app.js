320035ca97bc90babaac6b5160a926f296aa12b7
Swati Namdev <chirag awasthi>
2023-05-21 11:18:05 +0530

    test

--- app.js
+++ app.js
@@ -15,14 +15,15 @@ dotenv.config({path: "./config/config.env"})
 const app = express();
 
 const port = 2020;
-
+ 
 connectDB();
 
 // Middleware
 app.use(express.json());
 app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "*",
+      // origin: "http://localhost:3000",
       
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
